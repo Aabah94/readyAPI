9ce11302d339be49406b5e8f5d74823f9724350c
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2019-06-26 15:04:04 +0530

    Add files via upload

---
+++ src/main/java/com/smartbear/ready/plugin/template/factories/SampleDiscoveryMethod.java
@@ -0,0 +1,37 @@
+package com.smartbear.ready.plugin.template.factories;
+
+import com.eviware.soapui.impl.actions.DiscoveryMethod;
+import com.eviware.soapui.impl.rest.discovery.DiscoveredRequest;
+import com.eviware.soapui.model.workspace.Workspace;
+import com.eviware.soapui.plugins.auto.PluginDiscoveryMethod;
+import com.eviware.soapui.support.UISupport;
+
+import java.util.List;
+
+@PluginDiscoveryMethod
+public class SampleDiscoveryMethod implements DiscoveryMethod {
+    @Override
+    public boolean isSynchronous() {
+        return false;
+    }
+
+    @Override
+    public void discoverResources(Workspace workspace) {
+        UISupport.showInfoMessage("bu!");
+    }
+
+    @Override
+    public List<DiscoveredRequest> discoverResourcesSynchronously(Workspace workspace) {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Dummy Discovery method - doesn't actually do anything.";
+    }
+
+    @Override
+    public String getLabel() {
+        return "Ghost Discovery!";
+    }
+}
